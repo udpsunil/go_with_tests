464339e42ea4ccd102db77f63a94afea544b8a08
Sunil Udupi <udpsunil@gmail.com>
2024-09-06 21:06:47 +0530

    learn dictionary in go

---
+++ dictionary_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+
+	dictionary := map[string]string{"test": "this is just a test"}
+
+	got := Search(dictionary, "test")
+	want := "this is just a test"
+
+	assertStrings(got, want, t)
+}
+
+func assertStrings(got string, want string, t *testing.T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q, given %q", got, want, "test")
+	}
+}
