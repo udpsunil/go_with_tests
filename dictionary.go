fa30f02b331d4918f4475cd00eff264d7a773fb5
Sunil Udupi <udpsunil@gmail.com>
2024-09-11 21:41:52 +0530

    Refactor and Update functionality

--- dictionary.go
+++ dictionary.go
@@ -1,10 +1,17 @@
 package main
 
-import "errors"
-
 type Dictionary map[string]string
+type DictionaryErr string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it doesn't exist")
+)
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -17,4 +24,18 @@ func (d Dictionary) Search(word string) (string, error) {
 
 func (d Dictionary) Add(word, definition string) {
 	d[word] = definition
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+	return nil
 }
