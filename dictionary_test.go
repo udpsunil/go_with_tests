a6e03e9087a7d40fbfd46f371af717791fbf14ea
Sunil Udupi <udpsunil@gmail.com>
2024-09-11 21:16:58 +0530

    return error and refactor

--- dictionary_test.go
+++ dictionary_test.go
@@ -3,16 +3,28 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	dictionary := map[string]string{"test": "this is just a test"}
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+		assertStrings(t, got, want)
+	})
 
-	assertStrings(got, want, t)
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
-func assertStrings(got string, want string, t *testing.T) {
+func assertStrings(t *testing.T, got string, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q, want %q, given %q", got, want, "test")
